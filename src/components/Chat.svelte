49c94e872ad078cc69f40ac2864325b5f8085fdf
Minkyu Kim <kim774634@gmail.com>
2022-08-06 18:48:45 +0000

    Develop Chat input field

---
+++ src/components/Chat.svelte
@@ -0,0 +1,74 @@
+<script lang="ts">
+  import Ripple from '@smui/ripple';
+  import Icon from 'src/util/Icon.svelte';
+
+  let value = '';
+
+  function onSubmit() {
+    console.log(value);
+    value = '';
+  }
+</script>
+
+<form
+  on:submit|preventDefault={onSubmit}
+  class="chat">
+  <div class="chat-input">
+    <div use:Ripple={{ surface: true }}>
+      <input
+        type="text"
+        placeholder="Aa"
+        bind:value
+      />
+    </div>
+    <span use:Ripple={{ surface: true }}>
+      <button>
+        {#if value === ''}
+          <Icon name="send-outlined" width="23" height="23" />
+          {:else}
+            <Icon name="send-filled" width="23" height="23" />
+        {/if}
+      </button>
+    </span>
+  </div>
+</form>
+
+<style lang="scss">
+  .chat {
+    .chat-input {
+      position: relative;
+      div {
+        border-radius: 50px;
+        input {
+          cursor: pointer;
+          width: 100%;
+          height: 30px;
+          border-radius: 50px;
+          padding: 15px;
+          border: 1.5px solid $primary-color-default;
+          outline: none;
+          font-size: 1rem;
+        }
+      }
+      span {
+        position: absolute;
+        bottom: 50%;
+        right: 0;
+        transform: translate(0, 50%);
+        border-radius: 30px;
+        width: 32px;
+        height: 32px;
+        button {
+          width: 100%;
+          height: 100%;
+          background: transparent;
+          border: none;
+          cursor: pointer;
+          :global(svg) {
+            vertical-align: middle;
+          }
+        }
+      }
+    }
+  }
+</style>
---
+++ src/util/Icon.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  export let name: string;
+  export let width = '1rem';
+  export let height = '1rem';
+  export let focusable = false;
+  export let fill: string = '#4880EE';
+  const icons = [
+      {
+        box: 48,
+        name: 'send-outlined',
+        svg: `<path d="M6 40V8l38 16Zm3-4.65L36.2 24 9 12.5v8.4L21.1 24 9 27Zm0 0V12.5 27Z"/>`
+      },
+      {
+        box: 48,
+        name: 'send-filled',
+        svg: `<path d="M6 40V27.75L21.1 24 6 20.15V8l38 16Z"/>`
+      }
+  ];
+  const displayIcon: any = icons.find(e => e.name === name);
+</script>
+
+{#if displayIcon !== undefined}
+  <svg
+    class={$$props.class}
+    focusable={String(focusable)}
+    {width}
+    {height}
+    {fill}
+    viewBox="0 0 {displayIcon.box} {displayIcon.box}">{@html displayIcon.svg}</svg>
+    {:else}
+      <div>아이콘 없음</div>
+{/if}

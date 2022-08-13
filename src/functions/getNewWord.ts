8ea7b0d2866c0fb16dad763b8f258a4405e19e15
Minkyu Kim <kim774634@gmail.com>
2022-08-13 13:34:48 +0000

    채팅 보내기 및 받기 기능 구현

--- src/functions/getNewWord.ts
+++ src/functions/getNewWord.ts
@@ -5,11 +5,10 @@ import { api } from 'src/api';
 async function getNewWord(word: string) {
   const res = await api<'isExistWord'>('GET', `/api/isExistWord/${word}.json`);
   if (!res.existWord) {
-    console.log('존재하지 않는 단어입니다.');
-    return;
+    return '존재하지 않는 단어입니다.';
   }
 
-  console.log('존재하는 단어입니다.');
+  return '존재하는 단어입니다.';
 }
 
 export default getNewWord;

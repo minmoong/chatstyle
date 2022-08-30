9403725ce5c31929437d0e436767775527c5c529
Minkyu Kim <kim774634@gmail.com>
2022-08-30 15:38:24 +0000

    데이터베이스 이사

--- prisma/migrations/20220827165947_/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "RegionScore" (
-    "region" TEXT NOT NULL,
-    "score" BIGINT NOT NULL DEFAULT 0,
-    "moving" BOOLEAN NOT NULL DEFAULT false,
-
-    CONSTRAINT "RegionScore_pkey" PRIMARY KEY ("region")
-);
